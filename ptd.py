9f806a4b80dc771f4e4d316f5cedb451139b2899
ray <uubs@yeah.net>
2023-02-23 18:03:16 +0800

    init

--- ptd.py
+++ ptd.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
 # Define the neural network
 class DiceModel(nn.Module):
     def __init__(self):
@@ -14,29 +15,31 @@ class DiceModel(nn.Module):
         x = self.fc2(x)
         return x
 
+
 # Function to convert history to feature vector
 def history_to_feature(history):
     # Convert history to a binary matrix
     matrix = np.zeros((6, 3))
     for roll in history:
-        matrix[roll-1, :] = 1
+        matrix[roll - 1, :] = 1
 
     # Flatten matrix to a 1D array and return as PyTorch tensor
     return torch.tensor(matrix.flatten(), dtype=torch.float)
 
+
 # Load the training data
 data = [[3, 1, 1], [6, 5, 2], [4, 4, 4], [6, 3, 1], [6, 2, 1], [5, 3, 2], [5, 4, 3], [6, 4, 3], [6, 4, 2], [5, 3, 2],
-           [5, 4, 1], [5, 3, 1], [3, 2, 1], [6, 5, 4], [4, 4, 2], [6, 5, 4], [3, 3, 1], [5, 2, 1], [6, 6, 3], [6, 4, 3],
-           [4, 3, 3], [6, 4, 4], [2, 2, 1], [6, 5, 1], [4, 2, 2], [5, 2, 1], [2, 2, 1], [6, 3, 1], [4, 3, 1], [6, 2, 1],
-           [4, 4, 1], [5, 1, 1], [6, 5, 3], [5, 2, 1], [6, 6, 4], [4, 3, 3], [3, 2, 2], [5, 5, 2], [6, 6, 2], [6, 3, 2],
-           [3, 2, 2], [6, 5, 2], [4, 3, 2], [2, 1, 1], [5, 3, 2], [5, 3, 2], [3, 2, 2], [2, 2, 1], [5, 3, 2], [6, 5, 4],
-           [6, 4, 4], [6, 3, 1], [4, 3, 2], [6, 4, 2], [6, 4, 4], [4, 4, 3], [5, 5, 5], [4, 3, 1], [2, 2, 2], [5, 4, 3],
-           [6, 1, 1], [5, 2, 2], [2, 1, 1], [4, 2, 2], [6, 5, 1], [5, 4, 1], [6, 6, 1]]
+        [5, 4, 1], [5, 3, 1], [3, 2, 1], [6, 5, 4], [4, 4, 2], [6, 5, 4], [3, 3, 1], [5, 2, 1], [6, 6, 3], [6, 4, 3],
+        [4, 3, 3], [6, 4, 4], [2, 2, 1], [6, 5, 1], [4, 2, 2], [5, 2, 1], [2, 2, 1], [6, 3, 1], [4, 3, 1], [6, 2, 1],
+        [4, 4, 1], [5, 1, 1], [6, 5, 3], [5, 2, 1], [6, 6, 4], [4, 3, 3], [3, 2, 2], [5, 5, 2], [6, 6, 2], [6, 3, 2],
+        [3, 2, 2], [6, 5, 2], [4, 3, 2], [2, 1, 1], [5, 3, 2], [5, 3, 2], [3, 2, 2], [2, 2, 1], [5, 3, 2], [6, 5, 4],
+        [6, 4, 4], [6, 3, 1], [4, 3, 2], [6, 4, 2], [6, 4, 4], [4, 4, 3], [5, 5, 5], [4, 3, 1], [2, 2, 2], [5, 4, 3],
+        [6, 1, 1], [5, 2, 2], [2, 1, 1], [4, 2, 2], [6, 5, 1], [5, 4, 1], [6, 6, 1]]
 X = []
 y = []
 for i in range(len(data) - 1):
     X.append(history_to_feature(data[i]))
-    y.append(torch.tensor(data[i+1], dtype=torch.float))
+    y.append(torch.tensor(data[i + 1], dtype=torch.float))
 
 # Convert data to PyTorch tensors
 X = torch.stack(X)
