6a4274e124e0cb8cde24337695314f7b21cfd546
ray <uubs@yeah.net>
2023-02-23 15:29:11 +0800

    init

--- ptd.py
+++ ptd.py
@@ -1,52 +1,63 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import numpy as np
 
-# Generate training data
-num_samples = 10000
-roll_history_length = 10  # number of previous rolls to include in input
-X_train = np.random.randint(1, 7, size=(num_samples, roll_history_length, 3))
-y_train = np.sum(X_train, axis=(1, 2))  # target is sum of three rolls
-
-# Define a PyTorch model with one LSTM layer and one output layer
-class SicBoModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(SicBoModel, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
-        self.fc = nn.Linear(hidden_size, output_size)
+# Define the neural network
+class DiceModel(nn.Module):
+    def __init__(self):
+        super(DiceModel, self).__init__()
+        self.fc1 = nn.Linear(18, 20)
+        self.fc2 = nn.Linear(20, 6)
 
     def forward(self, x):
-        out, _ = self.lstm(x)
-        out = self.fc(out[:, -1, :])
-        return out
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
-model = SicBoModel(3, 32, 1)
+# Function to convert history to feature vector
+def history_to_feature(history):
+    # Convert history to a binary matrix
+    matrix = np.zeros((6, 3))
+    for roll in history:
+        matrix[roll-1, :] = 1
 
-# Define the loss function and optimizer
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # Flatten matrix to a 1D array and return as PyTorch tensor
+    return torch.tensor(matrix.flatten(), dtype=torch.float)
+
+# Load the training data
+data = [[3, 1, 1], [6, 5, 2], [4, 4, 4], [6, 3, 1], [6, 2, 1], [5, 3, 2], [5, 4, 3], [6, 4, 3], [6, 4, 2], [5, 3, 2],
+           [5, 4, 1], [5, 3, 1], [3, 2, 1], [6, 5, 4], [4, 4, 2], [6, 5, 4], [3, 3, 1], [5, 2, 1], [6, 6, 3], [6, 4, 3],
+           [4, 3, 3], [6, 4, 4], [2, 2, 1], [6, 5, 1], [4, 2, 2], [5, 2, 1], [2, 2, 1], [6, 3, 1], [4, 3, 1], [6, 2, 1],
+           [4, 4, 1], [5, 1, 1], [6, 5, 3], [5, 2, 1], [6, 6, 4], [4, 3, 3], [3, 2, 2], [5, 5, 2], [6, 6, 2], [6, 3, 2],
+           [3, 2, 2], [6, 5, 2], [4, 3, 2], [2, 1, 1], [5, 3, 2], [5, 3, 2], [3, 2, 2], [2, 2, 1], [5, 3, 2], [6, 5, 4],
+           [6, 4, 4], [6, 3, 1], [4, 3, 2], [6, 4, 2], [6, 4, 4], [4, 4, 3], [5, 5, 5], [4, 3, 1], [2, 2, 2], [5, 4, 3],
+           [6, 1, 1], [5, 2, 2], [2, 1, 1], [4, 2, 2], [6, 5, 1], [5, 4, 1], [6, 6, 1]]
+X = []
+y = []
+for i in range(len(data) - 1):
+    X.append(history_to_feature(data[i]))
+    y.append(torch.tensor(data[i+1], dtype=torch.float))
+
+# Convert data to PyTorch tensors
+X = torch.stack(X)
+y = torch.stack(y)
 
 # Train the model
-for epoch in range(50):
-    running_loss = 0.0
-    for i in range(num_samples):
-        inputs = torch.from_numpy(X_train[i:i+1, :, :]).float()
-        labels = torch.from_numpy(y_train[i:i+1]).float().unsqueeze(-1)
+model = DiceModel()
+# Define the loss function and optimizer
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-        optimizer.zero_grad()
+for epoch in range(1000):
+    optimizer.zero_grad()
+    output = model(X)
+    loss = loss_fn(output, y)
+    loss.backward()
+    optimizer.step()
 
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+    # Print progress
+    if epoch % 100 == 0:
+        print("Epoch:", epoch, "Loss:", loss.item())
 
-        running_loss += loss.item()
-
-    print(f"Epoch {epoch+1}, Loss: {running_loss/num_samples:.4f}")
-
-# Generate a prediction
-roll_history = np.array([[[1, 6, 2], [5, 3, 4], [6, 1, 2], [5, 4, 3], [2, 3, 1], [6, 6, 2], [1, 2, 5], [3, 5, 6], [1, 1, 1], [4, 5, 6]]])  # example history of previous rolls
-X_test = torch.from_numpy(roll_history).float()
-y_pred = model(X_test)
-print("Predicted sum of the next roll:", y_pred.item())
+# Save the trained model
+torch.save(model.state_dict(), '3dice_model.pth')
---
+++ sk_decision_tree_classifier.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+# @Time : 2023/2/23 下午2:28
+# @File : sk_decision_tree_classifier.py
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+
+# Load the training data
+# data = np.loadtxt('sicbo_data.csv', delimiter=',')
+# history = [[3, 1, 1], [6, 5, 2], [4, 4, 4], [6, 3, 1], [6, 2, 1], [5, 3, 2], [5, 4, 3], [6, 4, 3], [6, 4, 2], [5, 3, 2],
+#            [5, 4, 1], [5, 3, 1], [3, 2, 1], [6, 5, 4], [4, 4, 2], [6, 5, 4], [3, 3, 1], [5, 2, 1], [6, 6, 3], [6, 4, 3],
+#            [4, 3, 3], [6, 4, 4], [2, 2, 1], [6, 5, 1], [4, 2, 2], [5, 2, 1], [2, 2, 1], [6, 3, 1], [4, 3, 1], [6, 2, 1],
+#            [4, 4, 1], [5, 1, 1], [6, 5, 3], [5, 2, 1], [6, 6, 4], [4, 3, 3], [3, 2, 2], [5, 5, 2], [6, 6, 2], [6, 3, 2],
+#            [3, 2, 2], [6, 5, 2], [4, 3, 2], [2, 1, 1], [5, 3, 2], [5, 3, 2], [3, 2, 2], [2, 2, 1], [5, 3, 2], [6, 5, 4],
+#            [6, 4, 4], [6, 3, 1], [4, 3, 2], [6, 4, 2], [6, 4, 4], [4, 4, 3], [5, 5, 5], [4, 3, 1], [2, 2, 2], [5, 4, 3],
+#            [6, 1, 1], [5, 2, 2], [2, 1, 1], [4, 2, 2], [6, 5, 1], [5, 4, 1], [6, 6, 1]]
+history = [[5, 5, 1], [6, 5, 4], [5, 5, 5], [4, 4, 1], [5, 4, 3], [6, 4, 2], [6, 3, 1], [5, 2, 1], [3, 2, 1], [5, 2, 1],
+           [6, 3, 3], [6, 1, 1], [3, 2, 1], [6, 5, 4], [6, 4, 1], [5, 5, 4], [5, 2, 2], [5, 3, 2], [6, 4, 3], [6, 5, 3],
+           [6, 6, 5], [4, 4, 2], [6, 4, 2], [5, 4, 3], [6, 4, 1], [6, 5, 3], [5, 2, 1], [6, 5, 3], [5, 5, 2], [6, 5, 1],
+           [6, 3, 1], [6, 5, 4],]
+data = np.array(history)
+# Split the data into input (X) and target (y) arrays
+X = []
+y = []
+for i in range(len(data) - 2):
+    X.append(data[i])
+    y.append(data[i + 1])
+
+# Create a decision tree classifier and fit the data
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+# Get input from user (three dice rolls)
+rolls = history[-1]
+
+# Make a prediction
+pred = model.predict([rolls])[0]
+# Print the prediction
+print("The predicted outcome is:", pred)
