e3b771c39d50f1a6bec07d7fd7b82e9d17de46fc
ray <uubs@yeah.net>
2023-03-02 16:33:22 +0800

    init

--- monte_carlo.py
+++ monte_carlo.py
@@ -5,7 +5,7 @@ def roll_dice():
     return random.randint(1, 6)
 
 
-def predict_roll(history, num_trials=10000):
+def predict_roll(history, num_trials=1000):
     counts = [0] * 16
     for i in range(num_trials):
         roll1 = roll_dice()
@@ -19,29 +19,35 @@ def predict_roll(history, num_trials=10000):
     predicted_roll = random.choices(range(4, 20), probabilities)[0]
     return predicted_roll
 
-data = [[3, 1, 1], [6, 5, 2], [4, 4, 4], [6, 3, 1], [6, 2, 1], [5, 3, 2], [5, 4, 3], [6, 4, 3], [6, 4, 2], [5, 3, 2],
-        [5, 4, 1], [5, 3, 1], [3, 2, 1], [6, 5, 4], [4, 4, 2], [6, 5, 4], [3, 3, 1], [5, 2, 1], [6, 6, 3], [6, 4, 3],
-        [4, 3, 3], [6, 4, 4], [2, 2, 1], [6, 5, 1], [4, 2, 2], [5, 2, 1], [2, 2, 1], [6, 3, 1], [4, 3, 1], [6, 2, 1],
-        [4, 4, 1], [5, 1, 1], [6, 5, 3], [5, 2, 1], [6, 6, 4], [4, 3, 3], [3, 2, 2], [5, 5, 2], [6, 6, 2], [6, 3, 2],
-        [3, 2, 2], [6, 5, 2], [4, 3, 2], [2, 1, 1], [5, 3, 2], [5, 3, 2], [3, 2, 2], [2, 2, 1], [5, 3, 2], [6, 5, 4],
-        [6, 4, 4], [6, 3, 1], [4, 3, 2], [6, 4, 2], [6, 4, 4], [4, 4, 3], [5, 5, 5], [4, 3, 1], [2, 2, 2], [5, 4, 3],
-        [6, 1, 1], [5, 2, 2], [2, 1, 1], [4, 2, 2], [6, 5, 1], [5, 4, 1], [6, 6, 1]]
 
-print(predict_roll([sum(x) for x in data]))
-count_odd = 0
-count_small = 0
-for i in range(len(data)-1):
-    res = predict_roll([sum(x) for x in data[:i+1]])
-    r_odd = res % 2 == 1
-    r_small = res < 11
-    t_odd = sum(data[i+1]) % 2 == 1
-    t_small = sum(data[i+1]) < 11
-    if r_odd == t_odd:
-        count_odd += 1
-    if r_small == t_small:
-        count_small += 1
-odd_acc = count_odd / len(data)
-small_acc = count_small / len(data)
+data = [18,7,9,17,16,13,11,9,9,12,11,4,14,8,8,8,9,10,10,18,6,10,14,12,7,15,13,11,12,4,7,14,14,13,11,15,11,8,11,11,11,12,15,6,16,8,11,10,12]
+length = 35
+data = data[len(data)-length:]
+seed = 1
+max_epoch = 10
+count_poch = 0
+while count_poch < max_epoch:
+    count_odd = 0
+    count_small = 0
+    for i in range(length - 1):
+        res = predict_roll([x for x in data[:i + 1]])
+        r_odd = res % 2 == 1
+        r_small = res < 11
+        t_odd = data[i + 1] % 2 == 1
+        t_small = data[i + 1] < 11
+        if r_odd == t_odd:
+            count_odd += 1
+        if r_small == t_small:
+            count_small += 1
+    odd_acc = count_odd / length
+    small_acc = count_small / length
 
-print(odd_acc)
-print(small_acc)
+    if small_acc > 0.55 and odd_acc > 0.55:
+        break
+    else:
+        seed += 1
+        count_poch += 1
+
+print(predict_roll([x for x in data]))
+print(f"odd_acc: {odd_acc}, odd%: {sum([1 if i % 2 == 1 else 0 for i in data ])/len(data)}")
+print(f"small_acc: {small_acc}, small%: {sum([1 if i < 11 else 0 for i in data ])/len(data)}")
