d69027d5341db737e36836f56eb904fed9adebbf
ray <uubs@yeah.net>
2023-03-02 17:48:27 +0800

    update monte_carlo

--- monte_carlo_multi.py
+++ monte_carlo_multi.py
@@ -1,6 +1,7 @@
 import random
 import multiprocessing
 
+
 def roll_dice():
     return random.randint(1, 6)
 
@@ -22,21 +23,18 @@ def predict_roll(history, num_trials=1000):
 
 def predict_proc(pidx, lock):
     random.seed(random.seed(random.randint(1, 100)))
-    data = [18, 7, 9, 17, 16, 13, 11, 9, 9, 12, 11, 4, 14, 8, 8, 8, 9, 10, 10, 18, 6, 10, 14, 12, 7, 15, 13, 11, 12, 4,
-            7,
-            14, 14, 13, 11, 15, 11, 8, 11, 11, 11, 12, 15, 6, 16, 8, 11, 10, 12, 13, 13, 11, 12, 10, 11, 9, 8, 11, 12,
-            15,
-            12, 12, 11, 8, 15, 13, 13, 16, 9, 8, 9, 8, 12, 5, 11, 8, 9, 12, 8, 12, 9, 5, 13, 10, 8, 12, 12, 8, 14, 9, 9,
-            5, 16, 12, 5, 7, 9, 11, 11, 10, 13, 16, 4, 7]
+    data = [9, 12, 16, 10, 13, 12, 8, 12, 8, 11, 15, 15, 10, 9, 8, 11, 16, 12, 13, 11, 6, 13, 9, 11, 10, 9, 8, 12, 17,
+            16, 11, 10, 6, 6, 10, 10]
     length = 35
-    data = data[len(data) - length:]
+    if len(data) > length:
+        data = data[len(data) - length:]
     seed = 1
     max_epoch = 10
     count_poch = 0
     while count_poch < max_epoch:
         count_odd = 0
         count_small = 0
-        for i in range(length - 1):
+        for i, d in enumerate(data[:-1]):
             res = predict_roll([x for x in data[:i + 1]])
             r_odd = res % 2 == 1
             r_small = res < 11
